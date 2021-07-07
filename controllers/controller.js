f3becf974ab9ebe0f95cd19a0f78e1ba8b08071e
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2021-07-07 21:36:09 +0530

    added method to run python script using api

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,18 @@
+const { spawn } = require("child_process");
+
+const fileName = "hello-world.py";
 exports.listVideos = (req, res, next) => {
+  const data = spawn("python", [fileName]);
+  data.stdout.on("data", data => {
+    console.log(`stdout ${data}`);
+  });
+
+  data.stderr.on("data", data => {
+    console.log(`stderr ${data}`);
+  });
+
+  data.on("error", error => {
+    console.log(`Error ${error.message}`);
+  });
   res.send("ListVideos");
 };
